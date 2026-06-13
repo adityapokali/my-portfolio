8621bbd5517e614530a8c0e165951322c84ba757
Aditya Pokali <adityapokali18@gmail.com>
2026-06-13 23:25:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 function changeTheme(){
     document.body.classList.toggle("light-mode");
 }
+ScrollReveal({
+    distance: "60px",
+    duration: 1500,
+    delay: 200,
+    reset: false
+});
+
+// Hero Section
+ScrollReveal().reveal(".hero-image", {
+    origin: "left"
+});
+
+ScrollReveal().reveal(".hero-content", {
+    origin: "right"
+});
+
+// About Section
+ScrollReveal().reveal(".about-left", {
+    origin: "left"
+});
+
+ScrollReveal().reveal(".about-card", {
+    origin: "right"
+});
+
+// Skill Cards
+ScrollReveal().reveal(".skill-box", {
+    origin: "bottom",
+    interval: 200
+});
+
+// Project & Contact Cards
+ScrollReveal().reveal("#projects, #contact", {
+    origin: "bottom"
+});
